8350102b70186b850def207aad7c2995c86c36e2
GiantSpaceHead <142571165+GiantSpaceHead@users.noreply.github.com>
2023-09-05 12:42:50 +1000

    Add files via upload

---
+++ StyleLight2D.gd
@@ -0,0 +1,89 @@
+extends PointLight2D
+class_name StyleLight2D
+
+
+@export var Base_Energy = float(1)
+
+@export var Wait = float(0.5)
+
+@export var Strobe_Amount = float(1) 
+
+enum SArray {STATIC, STROBE, FLICKER}
+
+@export var Style = SArray.STATIC
+
+#	The Styles are all sorted into methods for readability
+
+func Static():
+	energy = Base_Energy
+
+
+#	Light effects are made by manipulating node propertys using tweens
+#	instead of using the tweens set_loops() function to make the tweens loop indfinitly
+#	I check to see if Style var has changed, if it hasn't, I use tween_callback to call the method again,
+#	if it has changed, I use the kill function on the tween.
+#	this way if I change the Light Style, the tween will not keep repeating.
+
+func Strobe():
+	var tween = create_tween()
+	
+	tween.tween_property(self, "energy", energy - Strobe_Amount, Wait)
+	tween.tween_property(self, "energy", Base_Energy, Wait)
+	
+	
+	
+	if not Style == SArray.STROBE:
+		tween.kill()
+	else:
+		tween.tween_callback(Strobe)
+
+
+func Flicker():
+	
+	energy = Base_Energy - 0.1
+	await create_tween().set_loops().tween_interval(0.1).finished
+	energy = Base_Energy
+	await create_tween().set_loops().tween_interval(0.1).finished
+	energy = Base_Energy - 0.2
+	await create_tween().set_loops().tween_interval(0.1).finished
+	energy = Base_Energy + 0.1
+	await create_tween().set_loops().tween_interval(0.1).finished
+	energy = Base_Energy - 0.3
+	await create_tween().set_loops().tween_interval(0.1).finished
+	
+	if Style == SArray.FLICKER:
+		Flicker()
+
+
+#	The methods above are called using this method below.
+
+func StyleFSM(Set_Style):
+	
+	
+	if not Set_Style == null:
+		Style = Set_Style
+	
+	
+	match Style:
+		SArray.STATIC:
+			energy = Base_Energy
+		SArray.STROBE:
+			Strobe()
+			
+		SArray.FLICKER:
+			
+			var FR = randf_range(0.1,1)
+			var tween = create_tween()
+			#tween and rand_range used to create a delay before Flicker method
+			#to desync it from other StyleLights at the start of the scene
+			
+			tween.tween_interval(FR)
+			tween.tween_callback(Flicker)
+
+
+#	The method StyleFSM is used called once at the start of a scene.
+#	This method can later be called again to change the Style.
+
+func _ready():
+	Base_Energy = energy
+	StyleFSM(Style)
